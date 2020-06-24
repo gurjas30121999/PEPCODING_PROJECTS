8f61d3d93ac596480a64b45042c4a9ed78fc8fd9
Singh_Gurjas <57052604+gurjas30121999@users.noreply.github.com>
2020-06-24 17:13:53 +0530

    Update news.js

--- AUTOMATED-SHEDULED-NEWS-CRAWLER/news.js
+++ AUTOMATED-SHEDULED-NEWS-CRAWLER/news.js
@@ -1,33 +1,32 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs");
+const puppeteer=require("puppeteer");
+const fs=require("fs");
 const schedule=require("node-schedule");
 const gather=async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
+    const browser=await puppeteer.launch({ headless: false });
+    const page=await browser.newPage();
     await page.setDefaultTimeout(0);
-    const newsTitles = await getNewsTitles(page);
-    
+    const newsTitles=await getNewsTitles(page);
     
     fs.writeFile("newsitems.json", JSON.stringify(newsTitles), "utf8", (err, data) => {
-        if (err) {
+        if(err){
         console.error(err);
-      } else {
+      }else{
         console.log("created!");
       }});
     await browser.close();
     };
 
     
-    async function getNewsTitles(page) {
+    async function getNewsTitles(page){
         await page.goto("https://www.thetimes.co.uk/?region=global");
-        return (results = await page.evaluate(() => {
-      const allTitles = document.querySelectorAll(".Item-headline a", {waitUntil: 'networkidle2'});
+        return (results=await page.evaluate(() =>{
+      const allTitles=document.querySelectorAll(".Item-headline a",{waitUntil:'networkidle2'});
           return Array.from(allTitles)
-            .slice(0, 10)
-            .map(title => {
-              let res = {
-                title: title.textContent,
-                link: title["href"]
+            .slice(0,10)
+            .map(title =>{
+              let res={
+                title:title.textContent,
+                link:title["href"]
               };
         
             return res;
